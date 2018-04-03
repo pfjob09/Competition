0f6ecf74ada1601ff2d8bc70cb39e1ce38f40f46
HuangQinJian <1115739835@qq.com>
2018-04-03 14:16:49 +0800

    update

--- AskandQuestion/AskandQuestion.py
+++ AskandQuestion/AskandQuestion.py
@@ -8,7 +8,6 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from sklearn.ensemble import BaggingRegressor
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 from xgboost import plot_tree
@@ -77,11 +76,101 @@ def load_testData(testFilePath):
     return X_testList
 
 
-def plot_feature(dataInput):
-    data = pd.read_csv(dataInput)
-    yList = data['answers'].groupby([data['year'], data['month']]).mean()
-    print(type(yList))
-    # print(yList.tolist())
+def feature_plot(FilePath):
+    data_train = pd.read_csv(FilePath)
+    # 将数据类型转换为日期类型
+    data_train['date'] = pd.to_datetime(data_train['date'])
+    # 将date设置为index
+    df = data_train.set_index('date')
+    # ===========================================================================================================
+    # 获取某年的数据
+    # df_some_year = df['2010']
+    # x_trick = np.arange(len(df_some_year))
+    # plt.plot(x_trick, df_some_year['questions'], label='questions')
+    # plt.plot(x_trick, df_some_year['answers'], label='answers')
+    # plt.title('2010的问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 获取某月的数据
+    # df_some_month = df['2013-11']
+    # x_trick = np.arange(len(df_some_month))
+    # plt.plot(x_trick, df_some_month['questions'], label='questions')
+    # plt.plot(x_trick, df_some_month['answers'], label='answers')
+    # plt.title('2013-11的问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 获取2015-12-31之前的数据
+    # df_after = df.truncate(after='2015-12-31')
+    # x_trick = np.arange(len(df_after))
+    # plt.plot(x_trick, df_after['questions'], label='questions')
+    # plt.plot(x_trick, df_after['answers'], label='answers')
+    # plt.title('2015-12-31之前的问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 获取2016-01-01以后的数据
+    # df_before = df.truncate(before='2016-01-01')
+    # x_trick = np.arange(len(df_before))
+    # plt.plot(x_trick, df_before['questions'], label='questions')
+    # plt.plot(x_trick, df_before['answers'], label='answers')
+    # plt.title('2016-01-01以后的问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 获取两段时间之间的数据
+    # df_between = df.truncate(before='2016-05-02', after='2016-08-15')
+    # x_trick = np.arange(len(df_between))
+    # plt.plot(x_trick, df_between['questions'], label='questions')
+    # plt.plot(x_trick, df_between['answers'], label='answers')
+    # plt.title('2016-05-02到2016-08-15之间的问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 获取两段时间之间的问题数，回答数差值
+    # df_between = df.truncate(before='2016-05-02', after='2016-08-15')
+    # x_trick = np.arange(len(df_between))
+    # plt.plot(x_trick, df_between['answers'] - df_between['questions'], label='差值')
+    # plt.title('2016-05-02到2016-08-15之间的问题数，回答数差值变化趋势')
+    # plt.legend(['差值'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 按年统计并显示
+    # df_year = df.resample('AS').sum().to_period('A')
+    # x_trick = np.arange(len(df_year))
+    # plt.plot(x_trick, df_year['questions'], label='questions')
+    # plt.plot(x_trick, df_year['answers'], label='answers')
+    # plt.title('每年问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 按季度统计并显示
+    # df_quarter = df.resample('Q').sum().to_period('Q')
+    # x_trick = np.arange(len(df_quarter))
+    # plt.plot(x_trick, df_quarter['questions'], label='questions')
+    # plt.plot(x_trick, df_quarter['answers'], label='answers')
+    # plt.title('每季度问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 按月度统计并显示
+    # df_month = df.resample('M').sum().to_period('M')
+    # x_trick = np.arange(len(df_month))
+    # plt.plot(x_trick, df_month['questions'], label='questions')
+    # plt.plot(x_trick, df_month['answers'], label='answers')
+    # plt.title('每月问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
+    # ===========================================================================================================
+    # 按周统计并显示
+    # df_week = df_week = df.resample('W').sum().to_period('W')
+    # x_trick = np.arange(len(df_week))
+    # plt.plot(x_trick, df_week['questions'], label='questions')
+    # plt.plot(x_trick, df_week['answers'], label='answers')
+    # plt.title('每周问题数，回答数变化趋势')
+    # plt.legend(['questions', 'answers'], loc=2)
+    # plt.show()
 
 
 def ceate_feature_map(features):
@@ -128,7 +217,7 @@ def model_process(X_train, Ques_trainList, Ans_trainList, X_test):
 if __name__ == '__main__':
     # deal_TrainInput(trainFilePath)
     # deal_TestInput(testFilePath)
-    # plot_feature(dealed_trainFilePath)
+    # feature_plot(trainFilePath)
     X_trainList, Ques_trainList, Ans_trainList = load_trainData(dealed_trainFilePath)
     X_testList = load_testData(dealed_testFilePath)
     model_process(X_trainList, Ques_trainList, Ans_trainList, X_testList)
